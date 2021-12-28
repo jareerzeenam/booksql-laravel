babcfdbdcdd6ee4a885799fcfe7deb813c966ea6
jareer <jareerzeenam@adventus.io>
2021-12-28 15:46:19 +0530

    Initial Commit

---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory;
+    
+    protected $suarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
